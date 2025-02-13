f372d0a16f0c94dd9929e76bb0bce5bed4ddad99
mdhamoda <98321950+mdhamoda@users.noreply.github.com>
2025-02-13 14:06:24 +0200

    Add files via upload

---
+++ doc/assets/search-idx.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        "title": "Home",
+        "fileName": "index.html",
+        "text": "Home Project Home Use the apexdox.homePagePath  setting to point to an HTML file that contains details about your project. The body of the HTML will show up here instead of this default!"
+    },
+    {
+        "title": "Sampleclass",
+        "fileName": "Sampleclass.html",
+        "text": "Sampleclass Sampleclass on Accounts that handles ensuring the correct system flags are set on our special accounts(Household, One-to-One), and also detects changes on Household Account that requires name updating. Signature public with sharing class Sampleclass Author Salesforce.com @date 2014 Sampleclass Methods samplemethod1() samplemethod2() Method return integer. Does specific function to calculate a x samplemethod1() Signature public void samplemethod1() samplemethod2() Method return integer. Does specific function to calculate a x Signature public integer samplemethod2()"
+    }
+];
